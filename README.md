0af8d98d1bde72d1728213af1d151969560a0905
Michel Maia <programador.michel.maia@gmail.com>
2022-10-21 22:07:36 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Efeito Digitacao com Javascript
- desenvolvido para aprendizado
+
+![preview](./.github/preview.png)
+
+
+Desenvolvido para aprendizado. Realizado efeito de digitaçãoo com Javascript. 21/10/22
+
+
+[🔗 Clique aqui para acessar] (https://michel-maia.github.io/Efeito-Digitação-com-Javascript/)
+
+
+## 💻 Tecnologias
+
+- HTML
+- CSS
+- Javascript
+
+
+## Referência
+
+- [Youtube] https://www.youtube.com/c/WillDev2022
+
+- [css-tricks] https://css-tricks.com/almanac/properties/w/whitespace/
---
+++ script.js
@@ -0,0 +1,22 @@
+var paragrafo = document.querySelector('p')
+var texto = paragrafo.innerHTML
+var index = 0
+
+const escrever = () => {
+    paragrafo.innerHTML = paragrafo.innerHTML.replace('|','')
+
+    if (texto.length > index) {
+        if (index == 0) {
+            paragrafo.innerHTML = texto.charAt(index)
+        } else {
+            paragrafo.innerHTML += texto.charAt(index)
+        }
+
+        paragrafo.innerHTML += '|'
+
+        index++
+        setTimeout(escrever, 50)
+    }
+}
+
+escrever()
